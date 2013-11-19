5b03a46adfd8bb1f5aed59927ef9dc53ef40bf1d
b1nj <postmaster@b1nj.fr>
2013-11-18 21:04:36 -0400

    Fix Issue #1 : where li in the description

--- js/jquery.b1njTimeline.js
+++ js/jquery.b1njTimeline.js
@@ -37,7 +37,7 @@
         this._duree = this._dateFin.diff(this._dateDebut);
 
         // Placement de l'évenement
-        this.$element.find('li').each(function () {
+        this.$element.find('ol > li').each(function () {
             $li = $(this);
             $li.wrapInner('<div class="event" />');
             var date = new moment($li.find('time').attr('datetime'), 'YYYY-MM-DD')
